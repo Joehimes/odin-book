d063af9cdb50ea09b3613779973cfb8b4e289668
Joseph Himes <joehimes42@gmail.com>
2019-08-13 17:50:04 -0400

    Added mutual friendship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  has_many :friend_requests, dependent: :destroy
+  has_many :pending_friends, through: :friend_requests, source: :friend
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
   has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def remove_friend(friend)
+    current_user.friends.destroy(friend)
+  end
 end
